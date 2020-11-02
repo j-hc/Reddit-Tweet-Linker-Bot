12ab115c9a22f30767e5afe410c9648d3543ef43
scrubjay55 <karayay12@gmail.com>
2020-11-02 13:11:48 +0300

    remove vision ocr completely

--- twitter_analyzing/__init__.py
+++ twitter_analyzing/__init__.py
@@ -3,5 +3,4 @@ from .err_reasons import Reasons
 from .TW_user_status import TWStatus
 from .tw_search_loop import TWSearch
 from .get_backup import Backup
-# from .ocr import OCR
 from .prep_text import TextPrep
