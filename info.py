e4011707947064a4d28208dd120ca12857210ddd
scrubjay55 <karayay12@gmail.com>
2021-01-12 15:03:29 +0300

    update info

--- info.py
+++ info.py
@@ -1,6 +1,7 @@
-useragent = 'tweetlinker by u/peroksizom'
-client_id = ""
-client_code = ""
+useragent = 'tweetlinker by u/tweetlinker'
+client_id = ''
+client_code = ''
 bot_username = 'tweetlinker'
-bot_pass = ""
-twitter_client_proxy = None  # twitter recognizes vps provider ip's and dont give the required guest token sometimes
+bot_pass = ''
+twitter_client_proxy = 'https://***'  # twitter recognizes vps provider ips and dont give the required guest token sometimes
+vision_ocr_api_key = ''
